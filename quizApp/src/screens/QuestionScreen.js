cf1e47dccaf4beaa32e6a05051a6a835ea94856b
Sadat Shahriar Bari <sadatshahriar@iut-dhaka.edu>
2021-02-13 01:04:21 +0600

    Input filed added in question screen

--- quizApp/src/screens/QuestionScreen.js
+++ quizApp/src/screens/QuestionScreen.js
@@ -10,6 +10,8 @@ import {
 	Dimensions,
 	Button,
 } from "react-native";
+import {Input,Card} from "react-native-elements";
+import {Entypo,FontAwesome } from "@expo/vector-icons";
 import * as firebase from "firebase";
 import "firebase/firestore";
 import { AuthContext } from "../providers/AuthProvider";
@@ -18,9 +20,27 @@ import { FlatList } from "react-native-gesture-handler";
 const { width } = Dimensions.get("window");
 
 const QuestionScreen = (props) => {
+	const [input, setInput] = useState("");
+
 	return (
 		<View>
-			<Text>Questions</Text>
+			
+			<Card>
+            <Input
+              placeholder="Type your question "
+              leftIcon={<FontAwesome name="pencil" size={24} color="black" />}
+              onChangeText={(currentText) => {
+                setInput(currentText);
+              }}
+            />
+            <Button
+              title="Post"
+              type="outline"
+              onPress={function () {
+				  console.log("post pressed");
+              }}
+            />
+          </Card>
 		</View>
 	);
 };
