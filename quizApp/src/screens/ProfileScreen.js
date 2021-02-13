0f6058f9bc6ed4b05eefbe4e0de25e3a98f3640f
Sadat Shahriar Bari <sadatshahriar@iut-dhaka.edu>
2021-02-13 19:03:47 +0600

    Profile Screen done

--- quizApp/src/screens/ProfileScreen.js
+++ quizApp/src/screens/ProfileScreen.js
@@ -1,18 +1,77 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import { View, Text,StyleSheet,Image,TouchableOpacity } from 'react-native';
+import {Card} from 'react-native-elements'
 import HeaderComponent from "./../components/HeaderComponent"
+import { AuthContext } from "./../providers/AuthProvider";
 
-const ProfileScreen=(props)=>{
-    return(
-        <View>
-             <HeaderComponent
-                DrawerFunction={() => {
-                    props.navigation.toggleDrawer();
-                }}
-                
-            />
-        </View>
+
+const ProfileScreen = (props) => {
+    return (
+
+        <AuthContext.Consumer>
+            {(auth) => (
+                <View style={styles.viewStyle}>
+                    { <HeaderComponent
+                        DrawerFunction={() => {
+                            props.navigation.toggleDrawer();
+                           
+                        }}
+                    />}
+
+                    <TouchableOpacity
+						onPress={function () {
+							console.log("Profile Pic Will be here")
+                            alert("User profile will be added here")
+						}}
+					>
+						<Image
+							style={styles.imageStyle}
+							source={require("./../../assets/user.png")}
+						/>
+					</TouchableOpacity>
+                    <Card>
+                        <View style={{ flexDirection: "column" }}>
+                           
+                            <Text style={styles.textStyle}>
+                               Name :  {auth.currentUser.displayName}  
+                           </Text>
+
+                        </View>
+                    </Card>
+
+                    <Card>
+                        <View style={{ flexDirection: "column" }}>
+                           
+                            <Text style={styles.textStyle}>
+                               Email :  {auth.currentUser.email}  
+                           </Text>
+                           
+                        </View>
+                    </Card>
+                </View>
+            )}
+        </AuthContext.Consumer>
+
+
     )
 }
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 20,
+        color: "black",
+        paddingHorizontal: 10
+    },
+    viewStyle: {
+        flex: 1,
+    },
+    imageStyle: {
+		alignSelf: "center",
+		height: 200,
+		width: 200,
+		marginVertical: 10,
+	},
+});
+
 
 export default ProfileScreen;
